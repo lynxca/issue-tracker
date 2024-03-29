f6b593b217924cb0afc0c013af7a49a87992908a
sheng <youtuw@yahoo.com>
2024-03-29 16:59:53 -0400

    build issue status badge

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,15 +1,3 @@
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
-import React from "react";
-
-function IssuesPage() {
-  return (
-    <div>
-      <Button>
-        <Link href="/issues/new">New Issue</Link>
-      </Button>
-    </div>
-  );
-}
+import { IssuesPage } from "./IssuesPage.1";
 
 export default IssuesPage;
