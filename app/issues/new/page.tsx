9128a599f316ea6e5d308b312776e7260e9f50bd
sheng <youtuw@yahoo.com>
2024-03-30 17:59:38 -0400

    disable server-side rendering

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,20 @@
 "use client";
-
+import dynamic from "next/dynamic";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 import { createIssueSchema } from "@/app/ValidationSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
-import delay from "delay";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-import dynamic from "next/dynamic";
 import { Controller, useForm } from "react-hook-form";
 import { z } from "zod";
 
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {ssr: false});
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
