b3207ee07442c7bb84f32db191c14006f4bac9b2
sheng <youtuw@yahoo.com>
2024-03-31 18:20:04 -0400

    add a delete button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "@radix-ui/themes/styles.css";
 import "./theme-config.css";
-import { Theme } from "@radix-ui/themes";
+import { Container, Theme } from "@radix-ui/themes";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body>
         <Theme radius="large">
           <NavBar />
-          <main className="p-5">{children}</main>
+          <Container>
+            <main className="p-5">{children}</main>
+          </Container>
         </Theme>
       </body>
     </html>
