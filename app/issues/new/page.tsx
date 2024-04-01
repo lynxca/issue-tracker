035acc514eb92b4a499cd2dbd8aabb4108d31c6a
sheng <youtuw@yahoo.com>
2024-04-01 12:33:13 -0400

    use supabase

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,6 @@
 import dynamic from "next/dynamic";
 import IssueFormSkeleton from "../_components/IssueFormSkeleton";
+import delay from "delay";
 
 const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
   ssr: false,
